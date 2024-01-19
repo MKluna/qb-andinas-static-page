1336cc063f8deab83853f85f26d0bde15d92256f
MKluna <matiaslunastyles@gmail.com>
2024-01-18 23:42:39 -0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mkluna.github.io/qb-andinas-static-page/',
