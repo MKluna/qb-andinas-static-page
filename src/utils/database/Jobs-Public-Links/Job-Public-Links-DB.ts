c39b7edb4f49d0c960866aca327d44af08d39770
MKluna <matiaslunastyles@gmail.com>
2024-01-22 00:16:45 -0300

    Fix NavBar URL

--- src/utils/database/Jobs-Public-Links/Job-Public-Links-DB.ts
+++ src/utils/database/Jobs-Public-Links/Job-Public-Links-DB.ts
@@ -1,5 +1,6 @@
 export const MockJobLinksDB = {
     NavbarJLogoHref: "/qb-andinas-static-page/./logo.png", // Cambiar aca cuando no se aloje en GithubPage
     NavbarJLogoTransparentHref: "/qb-andinas-static-page/./transparent-logo.png", // Cambiar aca cuando no se aloje en GithubPage
+    NavbarJHref: "/qb-andinas-static-page", // Cambiar aca cuando no se aloje en GithubPage
     JobsHeroJHref: "/qb-andinas-static-page/./jobs.jpg" // Cambiar aca cuando no se aloje en GithubPage
 }
