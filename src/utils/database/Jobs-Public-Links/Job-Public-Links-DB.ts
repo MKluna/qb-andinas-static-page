bcbe511720da6cc79ba612479b66e7cc58c840c9
MKluna <matiaslunastyles@gmail.com>
2024-01-22 00:10:01 -0300

    Fix Logo Job

--- src/utils/database/Jobs-Public-Links/Job-Public-Links-DB.ts
+++ src/utils/database/Jobs-Public-Links/Job-Public-Links-DB.ts
@@ -1,4 +1,5 @@
 export const MockJobLinksDB = {
     NavbarJLogoHref: "/qb-andinas-static-page/./logo.png", // Cambiar aca cuando no se aloje en GithubPage
+    NavbarJLogoTransparentHref: "/qb-andinas-static-page/./transparent-logo.png", // Cambiar aca cuando no se aloje en GithubPage
     JobsHeroJHref: "/qb-andinas-static-page/./jobs.jpg" // Cambiar aca cuando no se aloje en GithubPage
 }
