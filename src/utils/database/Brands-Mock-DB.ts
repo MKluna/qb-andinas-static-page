0c0e8dcc09877785d89b867366d9ae4f8095dce9
MKluna <matiaslunastyles@gmail.com>
2024-02-05 10:33:59 -0300

    Fix Links - 3

--- src/utils/database/Brands-Mock-DB.ts
+++ src/utils/database/Brands-Mock-DB.ts
@@ -1,12 +1,12 @@
 export const MockBrandsDB = {
     title: "Con la confianza de empresas como",
     logos: [
-        { src: "./brands/brand-1.png", width: 500, height: 450, alt: "brand-1" },
-        { src: "./brands/brand-2.png", width: 500, height: 450, alt: "brand-2" },
-        { src: "./brands/brand-3.png", width: 500, height: 450, alt: "brand-3" },
-        { src: "./brands/brand-4.png", width: 500, height: 450, alt: "brand-4" },
-        { src: "./brands/brand-5.png", width: 500, height: 450, alt: "brand-5" },
-        { src: "./brands/brand-6.png", width: 500, height: 450, alt: "brand-6" },
-        { src: "./brands/brand-7.png", width: 500, height: 450, alt: "brand-7" },
+        { src: "/qb-andinas-static-page/./brands/brand-1.png", width: 500, height: 450, alt: "brand-1" },
+        { src: "/qb-andinas-static-page/./brands/brand-2.png", width: 500, height: 450, alt: "brand-2" },
+        { src: "/qb-andinas-static-page/./brands/brand-3.png", width: 500, height: 450, alt: "brand-3" },
+        { src: "/qb-andinas-static-page/./brands/brand-4.png", width: 500, height: 450, alt: "brand-4" },
+        { src: "/qb-andinas-static-page/./brands/brand-5.png", width: 500, height: 450, alt: "brand-5" },
+        { src: "/qb-andinas-static-page/./brands/brand-6.png", width: 500, height: 450, alt: "brand-6" },
+        { src: "/qb-andinas-static-page/./brands/brand-7.png", width: 500, height: 450, alt: "brand-7" },
     ]
 }
--- src/utils/database/Hero-Mock-DB.ts
+++ src/utils/database/Hero-Mock-DB.ts
@@ -3,6 +3,6 @@ export const MockHeroDB = {
     principalColorText: "COMPROMISO",
     secondPrincipalText: "que llega.",
     paragraphText: "La empresa fue fundada en junio de 1984 con el compromiso de sus asociados y la dedicación constante de su personal.Poseemos la experiencia y capacitación necesarias para abordar proyectos,emprendimientos, obras y tareas vinculadas a auditoríasexternas, distribución postal, asesoramiento en normas y tendidos aéreos.",
-    srcHeroImage: './hero/hero.jpg',
+    srcHeroImage: '/qb-andinas-static-page/./hero/hero.jpg',
     altHeroImage: 'hero-img'
  }
