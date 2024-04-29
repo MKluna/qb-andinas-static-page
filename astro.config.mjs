e2cd7f079488298b0ee48d1453341da2cd2c3c1b
MKluna <matiaslunastyles@gmail.com>
2024-04-29 10:34:02 -0300

    Update trailingSlash

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,12 @@ const isBuild = SCRIPT.includes("astro build");
 
 let BASE_URL = isBuild ? LIVE_URL : LOCALHOST_URL;
 let BASE_PATH = isBuild ? '/qb-andinas-static-page/' : '/';
+let BASE_TRAILING_SLASH = isBuild ? 'always' : 'ignore';
 
 export default defineConfig({
   server: { port: SERVER_PORT },
   site: BASE_URL,
   base: BASE_PATH,
-  trailingSlash: "always",
+  trailingSlash: BASE_TRAILING_SLASH,
   integrations: [tailwind()]
 });
