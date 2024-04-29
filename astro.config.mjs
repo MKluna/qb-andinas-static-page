8b761adae5a53e1842e0af13e103658942debfce
MKluna <matiaslunastyles@gmail.com>
2024-04-28 22:05:59 -0300

    Test Base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-const SERVER_PORT = 4321
+const SERVER_PORT = 4321;
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 const LIVE_URL = "https://mkluna.github.io";
-const SCRIPT = process.env.npm_lifecycle_event || ""
+const SCRIPT = process.env.npm_lifecycle_event || "";
 const isBuild = SCRIPT.includes("astro build");
 
-let BASE_URL = LOCALHOST_URL;
-
-
-
-if (isBuild) {
-  BASE_URL = LIVE_URL + "/";
-}
+let BASE_URL = isBuild ? LIVE_URL : LOCALHOST_URL;
+let BASE_PATH = isBuild ? '/qb-andinas-static-page/' : '/';
 
 export default defineConfig({
   server: { port: SERVER_PORT },
   site: BASE_URL,
-  base: isBuild ? '/qb-andinas-static-page/' : '/',
+  base: BASE_PATH,
   integrations: [tailwind()]
 });
