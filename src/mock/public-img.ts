a015f2500424aee7eacec9324643a56462da5a55
MKluna <matiaslunastyles@gmail.com>
2024-06-02 13:57:07 -0300

    QB-PAGE:v1.0.0

---
+++ src/mock/public-img.ts
@@ -0,0 +1,76 @@
+const URL = import.meta.env.BASE_URL;
+
+export const FAV_ICON_IMG = `${URL + 'faviconqb/fav32.ico'}`;
+
+export const SERVICES_IMG = {
+    "AGUA_URL": `${URL}servicios/agua.png`,
+    "AGUA_ALT": "imagen de agua",
+    "LUZ_URL": `${URL}servicios/logistica.png`,
+    "LUZ_ALT": "imagen de luz",
+    "GAS_URL": `${URL}servicios/luz.png`,
+    "GAS_ALT": "imagen de gas",
+    "LOGISTICA_URL": `${URL}servicios/tendido.png`,
+    "LOGISTICA_ALT": "imagen de logística",
+    "SERVICES_IMG_1": `${URL}servicios/services_img_1.webp`,
+    "ALT_SERVICES_IMG_1": "alt de la imagen de servicios 1"
+};
+
+
+export const COMPANY_IMG = {
+    "SLIDE_1": `${URL + 'company/slide1.webp'}`,
+    "SLIDE_2": `${URL + 'company/slide2.webp'}`,
+    "ALT_SLIDE_1": "slide-company-1",
+    "ALT_SLIDE_2": "slide-company-2",
+}
+
+export const CONSULTING_IMG = {
+    "CONTROL_CALIDAD_URL": `${URL}consulting/control_de_calidad.png`,
+    "CONTROL_CALIDAD_ALT": "control-calidad",
+    "DESARROLLO_HUMANO_URL": `${URL}consulting/desarrollo_humano.png`,
+    "DESARROLLO_HUMANO_ALT": "desarrollo-humano",
+    "SEGURIDAD_SALUD_URL": `${URL}consulting/seguridad_y_salud.png`,
+    "SEGURIDAD_SALUD_ALT": "seguridad-y-salud",
+    "SISTEMA_DE_GESTION_URL": `${URL}consulting/sistema_de_gestion.png`,
+    "SISTEMA_DE_GESTION_ALT": "sistema-de-gestion"
+};
+
+
+
+export const EXPERIENCE_IMG = [
+    {
+        url: `${URL}experience/img-1.webp`
+    },
+    {
+        url: `${URL}experience/img-2.webp`
+    },
+    {
+        url: `${URL}experience/img-3.webp`
+    },
+    {
+        url: `${URL}experience/img-4.webp`
+    },
+    {
+        url: `${URL}experience/img-5.webp`
+    },
+    {
+        url: `${URL}experience/img-6.webp`
+    },
+    {
+        url: `${URL}experience/img-7.webp`
+    },
+    {
+        url: `${URL}experience/img-8.webp`
+    },
+    {
+        url: `${URL}experience/img-9.webp`
+    },
+    {
+        url: `${URL}experience/img-10.webp`
+    },
+    {
+        url: `${URL}experience/img-11.webp`
+    },
+    {
+        url: `${URL}experience/img-12.webp`
+    }
+];
--- src/utils/database/Services-Public-Links/Services-Public-Links-DB.ts
+++
@@ -1,5 +0,0 @@
-const URL = import.meta.env.BASE_URL;
-
-export const MockServicesLinksDB = {
-    ServicesHeroJHref: URL + "services/services.webp"
-}
