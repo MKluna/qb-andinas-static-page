5615d11f2a65e9a36db4d144962351388b0ff5ea
MKluna <matiaslunastyles@gmail.com>
2024-06-18 00:51:51 -0300

    QB-PAGE:v2.1.4

--- src/mock/public-txt.ts
+++ src/mock/public-txt.ts
@@ -1,21 +1,22 @@
 const URL = import.meta.env.BASE_URL;
 
 export const CONSULTIN_TXT = {
-    "TITLE": "Consultoria",
-    "SEGURIDAD_Y_SALUD": "Inducción a la seguridad\nPrevención y lucha contra incendios\nRiesgo eléctrico\nTrabajo en altura",
+    "TITLE": "Consultoría",
+    "SEGURIDAD_Y_SALUD":  ["Inducción a la seguridad", "Prevención y combate de incendios", "Riesgo eléctrico", "Trabajo en alturas"],
     "SISTEMA_DE_GESTION": {
         "TITLE": "Capacitación y asesoramiento",
-        "ITEM1": "- Implementación de sistemas de gestión ISO 9001, ISO 14001 e ISO 45001",
-        "ITEM2": "- Auditorías internas en ISO 9001, ISO 14001 e ISO 45001"
+        "ITEM1": "Implementación de sistemas de gestión ISO 9001, ISO 14001 e ISO 45001",
+        "ITEM2": "Auditorías internas en ISO 9001, ISO 14001 e ISO 45001"
     },
-    "DESARROLLO_HUMANO": "Invertir en el desarrollo humano es esencial para el éxito sostenible de las empresas y el bienestar de sus empleados.\nEl mismo fomenta la autoconciencia, la autoestima y el crecimiento individual, lo que beneficia tanto a los empleados como a la empresa.",
+    "DESARROLLO_HUMANO": "Invertir en el desarrollo humano es esencial para el éxito sostenible de las empresas y el bienestar de sus empleados. Fomenta la autoconciencia, la autoestima y el crecimiento individual, beneficiando tanto a los empleados como a la empresa.",
     "CONTROL_DE_CALIDAD": {
         "TITLE": "Asesoramiento",
-        "ITEM1": "- Confección de instructivos y procedimientos",
-        "ITEM2": "- Detección de anomalías, análisis de causa y acciones a seguir",
-        "ITEM3": "- Diagramas de reiteración y gravedad"
+        "ITEM1": "Elaboración de instructivos y procedimientos",
+        "ITEM2": "Detección de anomalías, análisis de causas y acciones a seguir",
+        "ITEM3": "Diagramas de correlación y gravedad"
     }
-}
+};
+
 
 export const HERO_TXT = {
     "TITLE_SECOND_SLIDE": "COMPROMISO QUE LLEGA.",
